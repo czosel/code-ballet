5ebc900081e88a9e8e688ac343981185e26ba9ec
Raphael Knaus <rknuus@gmail.com>
2016-06-04 21:42:05 +0200

    add printing of board

---
+++ 2016-06-20-tower-of-hanoi/rknuus/tanoi.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+
+# TODO(KNR): enforce Python 3
+
+import itertools
+
+
+class Queue(object):
+    def __init__(self):
+        self._items = []
+
+    def __bool__(self):
+        return bool(self._items)
+
+    def enqueue(self, item):
+        self._items.insert(0, item)
+
+    def dequeue(self):
+        return self._items.pop()
+
+
+class Stack(object):
+    def __init__(self):
+        self._items = []
+
+    def __bool__(self):
+        return bool(self._items)
+
+    def push(self, item):
+        self._items.push(item)
+
+    def pop(self):
+        return self._items.pop()
+
+
+class Board(object):
+    @classmethod
+    def start(cls, height):
+        cls._height = height  # actually equals to len(home) + len(interim) + len(target)
+        return Board(range(0, height), [], [])
+
+    def __init__(self, home, interim, target):
+        self._home = home
+        self._interim = interim
+        self._target = target
+
+    def moves(self):
+        pass
+
+    def is_solved(self):
+        return False
+
+    @staticmethod
+    def _get_column_width(disk):
+        return 1 + (disk) * 2
+
+    @staticmethod
+    def _get_row(a, b, c):
+        column_width = Board._get_column_width(Board._height) + 2  # add padding
+        return (('-' * Board._get_column_width(a)).center(column_width) + '|' +
+                ('-' * Board._get_column_width(b)).center(column_width) + '|' +
+                ('-' * Board._get_column_width(c)).center(column_width))
+
+    def __str__(self):
+        return '\n'.join(Board._get_row(a, b, c) for a, b, c in itertools.zip_longest(self._home, self._interim, self._target, fillvalue=-1))
+
+
+def solve(n):
+    initial = Board.start(n)
+    queue = Queue()
+    queue.enqueue(initial)
+    while queue:
+        board = queue.dequeue()
+        if board.is_solved():
+            print(board)
+            break
+        for move in board.moves():
+            queue.enqueue(move)
---
+++ 2016-06-20-tower-of-hanoi/rknuus/test_tanoi.py
@@ -0,0 +1,16 @@
+from tanoi import Board
+
+
+def test_print_initial_boards():
+    single = Board.start(1)
+    assert str(single) == '  -  |     |     '
+
+    double = Board.start(2)
+    assert str(double) == '   -   |       |       \n  ---  |       |       '
+
+    fiver = Board.start(5)
+    assert str(fiver) == ('      -      |             |             \n'
+                          '     ---     |             |             \n'
+                          '    -----    |             |             \n'
+                          '   -------   |             |             \n'
+                          '  ---------  |             |             ')
